f89af2d9ff69e4500439dfbae015a0e6f55e0c9f
Todd Gehman <toddgehman@gmail.com>
2011-06-05 14:01:16 -0700

    twiddles to docs and css

--- abaci.js
+++ abaci.js
@@ -6,6 +6,16 @@
 * 
 * abaci is distributed under the Do What The Fuck You Want To Public License
 * http://sam.zoy.org/wtfpl/ 
+
+// Rough development plan:
+// 0.2.0 side effect items of item in focus should be highlighted
+// 0.3.0 elements can be locked and unlocked
+// 0.4.0 some elements have mutually exclusive locking
+// 0.5.0 show results continuously in real time, with placeholders when number is incalculable, etc
+// 0.6.0 english units show the total and also the divider that would be used to measure them (with icons)
+// 0.7.0 fractions are handled elegantly - they do what a human would expect
+// 0.8.0 dependencies can be assigned individually or as chains
+// 0.9.0 chains can be directional and "bounce" based on locking
 */
 
 var abaci = {}
@@ -39,11 +49,3 @@ abaci.Calculator.prototype._labelFor = function($input_element) {
   return $input_element.parent().find('label[for=' + $input_element.attr('name') + ']');
 }
 
-// * side effect items of item in focus should be highlighted
-
-// * elements can be locked and unlocked
-// * some elements have mutually exclusive locking
-// * create example calculator using dog years
-// * show results continuously in real time, with placeholders when number is incalculable, etc
-// * english units show the total and also the divider that would be used to measure them (with icons)
-// * fractions are handled elegantly - they do what a human would expect
